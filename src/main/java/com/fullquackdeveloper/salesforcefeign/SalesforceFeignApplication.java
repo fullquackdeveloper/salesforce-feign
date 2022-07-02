12121ccec44b3f71afb3246ae26646096ac33730
fullquackdeveloper <fullquackdeveloper@gmail.com>
2022-07-03 00:30:46 +0800

    Initial commit

---
+++ src/main/java/com/fullquackdeveloper/salesforcefeign/SalesforceFeignApplication.java
@@ -0,0 +1,13 @@
+package com.fullquackdeveloper.salesforcefeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforceFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesforceFeignApplication.class, args);
+    }
+
+}
